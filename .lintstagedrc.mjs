35293998c2631fa9d48b36d3a32de598e0d98755
dafengzhen <dafengzhen@aliyun.com>
2024-12-30 01:03:31 +0800

    chore: update package.json

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -3,6 +3,9 @@ import path from 'path';
 const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`;
 
-module.exports = {
+const lintStagedConfig = {
+  '**/*': 'prettier --write --ignore-unknown',
   '*.{js,jsx,ts,tsx}': [buildEslintCommand],
 };
+
+export default lintStagedConfig;
